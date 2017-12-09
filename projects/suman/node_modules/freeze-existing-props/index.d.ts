51ea1678147e159846434a16e7f99b0effe74ea1
Olegzandr VD <alex@oresoftware.com>
2017-12-08 20:10:21 -0800

    pdev:set

--- projects/suman/node_modules/freeze-existing-props/index.d.ts
+++
@@ -1,5 +0,0 @@
-export interface IFEPStdObj {
-    [key: string]: any;
-}
-export declare const freezeExistingProps: ($obj: any, depth?: number) => any;
-export declare const freezeAllProps: ($obj: Object, depth?: number) => Object;
--- projects/suman/node_modules/log-prepend/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="node" />
-import { Writable } from "stream";
-export declare const lp: (str: string, strm: Writable, beforeHook: Function, afterHook: Function) => () => void;
